6333de3db327782cf6716ad3e0cac848bf5983a4
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-16 00:25:19 -0500

    created the model functions for get all cars and get car by id

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,26 @@
 // DO YOUR MAGIC
+
+const express = require("express");
+const router = express.Router();
+const Cars = require("./cars-model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get("/:id", async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id);
+    res.json(car);
+  } catch (err) {
+    next(err);
+  }
+});
+router.post("/", async (req, res, next) => {
+  res.json("posting a new car");
+});
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
-const express = require("express")
+const express = require("express");
+const carsRouter = require("./cars/cars-router");
+const server = express();
+server.use(express.json());
 
-const server = express()
+server.use("/api/cars", carsRouter);
 
-// DO YOUR MAGIC
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: " Not found " });
+});
 
-module.exports = server
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+module.exports = server;
